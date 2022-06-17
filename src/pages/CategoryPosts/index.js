321db13005261dea2dfe80dae7d40f25b33b304a
Mathias <106362276+immathiias@users.noreply.github.com>
2022-06-17 02:57:44 -0300

    Add files via upload

---
+++ src/pages/CategoryPosts/index.js
@@ -0,0 +1,81 @@
+import React, { useLayoutEffect, useEffect, useState } from "react";
+import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native'
+
+import { useNavigation, useRoute } from '@react-navigation/native'
+import api from '../../services/api'
+
+import PostItem from "../../components/PostItem";
+
+export default function CategoryPosts(){
+
+    const navigation = useNavigation();
+    const route = useRoute();
+
+    const [posts, setPosts] = useState([])
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            title: route.params?.title === '' ? 'Categoria' : route.params?.title
+        })
+    }, [navigation])
+
+    useEffect(() => {
+        async function loadPosts(){
+            const response = await api.get(`api/categories/${route.params?.id}?fields=name&populate=posts,posts.cover`)
+            setPosts(response.data?.data?.attributes?.posts?.data)
+        }
+
+        loadPosts();
+    }, [])
+
+    function handleBack(){
+        navigation.goBack();
+    }
+
+    return(
+        <View style = { styles.container }>
+
+            {posts.length === 0 && (
+                <View style={styles.warningCotainer}>
+                    <Text style={styles.warning}>Essa categoria ainda não possui nenhum post.</Text>
+                    <TouchableOpacity style={styles.backButton} onPress={handleBack}>
+                        <Text style={styles.textButton}>Encontrar posts</Text>
+                    </TouchableOpacity>
+                </View>
+            )}
+
+            <FlatList
+            style={{flex: 1}}
+            showsVerticalScrollIndicator={false}
+            data={posts}
+            keyExtractor={(item) => String(item.id)}
+            renderItem={({item}) => <PostItem data={item}/>}
+            />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        padding: 18,
+        backgroundColor: '#FFF'
+    },
+    warningCotainer:{
+        alignItems: 'center'
+    },
+    warning:{
+        fontSize: 16,
+        fontWeight: 'bold'
+    },
+    backButton:{
+        backgroundColor: '#162133',
+        paddingVertical: 8,
+        paddingHorizontal: 14,
+        marginTop: 12,
+        borderRadius: 4
+    },
+    textButton:{
+        color: '#FFF'
+    }
+})
